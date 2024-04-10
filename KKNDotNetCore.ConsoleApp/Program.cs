adc7a834ee93c6b6c817991b56ae157a71b9b0d3
kokonaing <WMBMP6xqKnjMpxXhHPkK>
2024-04-10 20:28:59 +0630

    Add project files.

---
+++ KKNDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+{
+    DataSource = "kkn" ,
+    InitialCatalog = "DotNetTesting" ,
+    UserID = "sa",
+    Password =  "root"
+};
+SqlConnection connection = new SqlConnection(builder.ConnectionString);
+connection.Open();
+
+string sql = "Select * From Tbl_Blog";
+SqlCommand cmd = new SqlCommand(sql, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+
+connection.Close();
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id => " + dr["Blog_Id"]);
+    Console.WriteLine("Blog Title => " + dr["Blog_Title"]);
+    Console.WriteLine("Blog Author => " + dr["Blog_Author"]);
+    Console.WriteLine("Blog Content => " + dr["Blog_Content"]);
+    Console.WriteLine("=========================");
+
+}
+
+Console.ReadKey();
