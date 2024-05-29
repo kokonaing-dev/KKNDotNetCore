bcfc6dc7b3655c2a4e4e8fd96db8d7eab1aa173c
kokonaing <kokonai1827@gmail.com>
2024-05-29 07:10:02 +0630

    [Complete] Refit CRUD

---
+++ KKNDotNetCore.ConsoleAppRefitExamples/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KKNDotNetCore.ConsoleAppRefitExamples;
+
+Console.WriteLine("Hello, World!");
+
+//try
+//{
+//    RefitExample refitExample = new RefitExample();
+//    await refitExample.RunAsync();
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.ToString());
+//}
+
+RefitExample refitExample = new RefitExample();
+await refitExample.RunAsync();
