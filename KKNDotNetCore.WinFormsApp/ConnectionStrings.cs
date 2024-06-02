3bd0efdb7f2328b1afe7a2ad4a6be928d5ad1e97
kokonaing <kokonai1827@gmail.com>
2024-06-03 06:11:02 +0630

    Uncloplete Windwo Form

---
+++ KKNDotNetCore.WinFormsApp/ConnectionStrings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKNDotNetCore.WinFormsApp
+{
+    public static class ConnectionStrings
+    {
+        public static readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTesting",
+            UserID = "sa",
+            Password = "root",
+            TrustServerCertificate = true
+        };
+    }
+}
