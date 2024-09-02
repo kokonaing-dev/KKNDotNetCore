1bb90153bd8b21017ff41166930b5671ea1d0d4b
kokonaing <kokonai1827@gmail.com>
2024-09-02 07:42:59 +0630

    [Complete] MVC Project Created

---
+++ KKNDotNetCore.MvcApp2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KKNDotNetCore.MvcApp2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
