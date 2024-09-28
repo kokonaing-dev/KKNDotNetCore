ca1ded13ed1902e01d8e1b891e301e4cbe2d64f8
kokonaing <kokonai1827@gmail.com>
2024-09-28 23:38:10 +0630

    [Complete] ChartJs and CanvasJs (charts)

---
+++ KKNDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KKNDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
---
+++ KKNDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KKNDotNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult ExmapleChart()
+        {
+            return View();
+        }
+        
+        public IActionResult InterpolationLineChart()
+        {
+            return View();
+        }
+
+
+
+    }
+}
