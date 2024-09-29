8f71462ad31f5f371957afee1083e3486f13f102
kokonaing <kokonai1827@gmail.com>
2024-09-29 12:30:14 +0630

    [Complete] Text Log & Db Log

--- KKNDotNetCore.MvcChartApp/Program.cs
+++ KKNDotNetCore.MvcChartApp/Program.cs
@@ -1,11 +1,16 @@
 using Serilog;
+using Serilog.Sinks.MSSqlServer;
 
 string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
     "logs/KKNDotNetCore.MvcChartApp.log");
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
-     .WriteTo.File(filePath, rollingInterval: RollingInterval.Hour)
+    .WriteTo.File(filePath, rollingInterval: RollingInterval.Hour)
+    .WriteTo
+    .MSSqlServer(
+        connectionString: "Server=.;Database=DotNetTraining;User ID=sa;Password=root;TrustServerCertificate=True;",
+        sinkOptions: new MSSqlServerSinkOptions { TableName = "Tbl_LogEvents" , AutoCreateSqlTable = true })
     .CreateLogger();
 
 try
