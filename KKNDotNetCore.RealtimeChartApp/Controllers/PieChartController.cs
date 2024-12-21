fcb6826f563d54bc1d0853b105501977b7de0757
kokonaing <kokonai1827@gmail.com>
2024-12-21 15:44:50 +0630

    feat: piechart views

---
+++ KKNDotNetCore.RealtimeChartApp/Controllers/PieChartController.cs
@@ -0,0 +1,35 @@
+﻿using KKNDotNetCore.RealtimeChartApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KKNDotNetCore.RealtimeChartApp.Controllers
+{
+    public class PieChartController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public PieChartController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(TblPieChart reqModel)
+        {
+            await _db.TblPieCharts.AddAsync(reqModel);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Create");
+        }
+
+    }
+}
---
+++ KKNDotNetCore.RealtimeChartApp/Models/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace KKNDotNetCore.RealtimeChartApp.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblPieChart> TblPieCharts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<TblPieChart>(entity =>
+        {
+            entity.HasKey(e => e.PieChartId);
+
+            entity.ToTable("Tbl_PieChart");
+
+            entity.Property(e => e.PieChartName)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.PieChartValue).HasColumnType("numeric(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
